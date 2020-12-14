37af63602f4f11cf99e633d47bc989bc4cd0d620
Vinícius Henrique Silveira <vhsilveira2@gmail.com>
2020-12-13 23:23:21 -0300

    refactor Vocabulary class

--- code/app.py
+++ code/app.py
@@ -21,27 +21,19 @@ class Text(Resource):
         return new_text, 201
 
 class Vocabulary(Resource):
-    def get(self):
+    def get(self, gram):
         word_list = []
         for text in texts:
-            word_list.extend(text['content'].split())
-        word_list = list(dict.fromkeys(word_list))
-        word_list = [word.translate(str.maketrans('', '', string.punctuation)) for word in word_list if word not in stopwords] # remove pontuação e stopwords
-        return ({'vocabulary': word_list})
-
-class VocabularyGram2(Resource):
-    def get(self):
-        word_list = []
-        for text in texts:
-            word_list.extend(ngrams(text['content'].split(), 2))
+            word_list.extend(ngrams(text['content']
+            .lower() # transforme tudo em lowercase para desconsiderar o cade
+            .replace('-', ' ') # substitui '-' por ' ' para considerar duas palavras no caso de palavras compostas (ex. mostre-me será 'mostre' e 'me' separadamente)
+            .split(), gram)) # aplica o n-gram conforme o valor de gram passado na request
         word_list = [' '.join(word) for word in word_list] # transforma de tuple para list
         word_list = list(dict.fromkeys(word_list))
         word_list = [word.translate(str.maketrans('', '', string.punctuation)) for word in word_list if word not in stopwords] # remove pontuação e stopwords
         return ({'vocabulary': word_list})
-        
 
 api.add_resource(Text, '/text')
-api.add_resource(Vocabulary, '/vocabulary')
-api.add_resource(VocabularyGram2, '/vocabulary2')
+api.add_resource(Vocabulary, '/vocabulary/<int:gram>')
 
 app.run(port=5000)
